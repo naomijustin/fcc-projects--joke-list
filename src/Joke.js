49299bc4f03e4736a670665202459c98a7be8506
naomijustin <naomi.z.justin@gmail.com>
2023-06-13 16:33:48 +1000

    Truthy rendering for punchline joke

--- src/Joke.js
+++ src/Joke.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Joke (props) {
     return(
     <div style={{margin: "10px 0px", padding: "20px"}}>
-        <b style={{display: props.setup ? "block" : "none"}}>{props.setup}</b>        
+        {props.setup && <b>{props.setup}</b>}
         <p>{props.punchline}</p>
         <hr />
     </div>
