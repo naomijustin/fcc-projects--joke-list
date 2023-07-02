3e3a12683710529c34843bf1f28ea18316708221
Naomi Justin <naomi.z.justin@gmail.com>
2023-07-02 15:00:29 +1000

    mapped elements and returned to dom, added key to each child

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from "react"
 import Joke from "./Joke.js"
+import jokesData from "./jokesData.js"
 
 function App () {
+  const jokeElements = jokesData.map(joke => {
+    return <Joke key={joke.jokeID} setup={joke.setup} punchline={joke.punchline}/>
+  });
+
   return (
     <div>
-      <Joke setup="Why did the chicken cross the road?" punchline="To get to the other side." />      
-      <Joke setup="What's the best thing about Switzerland?" punchline="I don't know, but the flag is a big plus." />      
-      <Joke setup="Helvetica and Times New Roman walk into a bar." punchline='"Get out of here!" shouts the bartender. "We don&#39;t serve your type."' />      
-      <Joke setup="Hear about the new restaurant called Karma?" punchline='There&#39;s no menu: You get what you deserve.' />      
-      <Joke setup="Did you hear about the actor who fell through the floorboards?" punchline='He was just going through a stage.' />      
-      <Joke punchline="My wife told me to stop impersonating a flamingo. I had to put my foot down." />      
+      {jokeElements}
     </div>
   );
 }
 
 export default App
+
+
+{/* <Joke setup="Why did the chicken cross the road?" punchline="To get to the other side." />      
+      <Joke setup="What's the best thing about Switzerland?" punchline="I don't know, but the flag is a big plus." />      
+      <Joke setup="Helvetica and Times New Roman walk into a bar." punchline='"Get out of here!" shouts the bartender. "We don&#39;t serve your type."' />      
+      <Joke setup="Hear about the new restaurant called Karma?" punchline='There&#39;s no menu: You get what you deserve.' />      
+      <Joke setup="Did you hear about the actor who fell through the floorboards?" punchline='He was just going through a stage.' />      
+      <Joke punchline="My wife told me to stop impersonating a flamingo. I had to put my foot down." /> */}
--- src/jokesData.js
+++ src/jokesData.js
@@ -1,21 +1,25 @@
 export default [
    {
+        jokeID: 1,
         setup: "I got my daughter a fridge for her birthday.",
         punchline: "I can't wait to see her face light up when she opens it."
     },
     {
+         jokeID: 2,
         setup: "How did the hacker escape the police?",
         punchline: "He just ransomware!"
     },
     {
-        setup: "Why don't pirates travel on mountain roads?",
+         jokeID: 3,        
         punchline: "Scurvy."
     },
     {
+         jokeID: 4,
         setup: "Why do bees stay in the hive in the winter?",
         punchline: "Swarm."
     },
     {
+         jokeID: 5,
         setup: "What's the best thing about Switzerland?",
         punchline: "I don't know, but the flag is a big plus!"
     }
